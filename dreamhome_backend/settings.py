3ee69b02883d4f766d5e6b4f4d9e37acf58cc130
Zach Alfred <arevalo.zachalfred123@gmail.com>
2026-03-29 23:12:16 +0800

    UPDATED REQUIREMENTS

--- dreamhome_backend/settings.py
+++ dreamhome_backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    
 ]
 
 MIDDLEWARE = [
@@ -79,12 +82,11 @@ WSGI_APPLICATION = 'dreamhome_backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres.vidjgnchetwdsqhhzmzs:tjnAFDSjRKM1B5gZ@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres',
+        conn_max_age=600
+    )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
