f915e6605d01cfabba7788cfe6d50e652222149b
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-30 20:59:18 +0800

    adding more on the set up of the back end

--- dreamhome_api/settings.py
+++ dreamhome_api/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v29h+@cqlwshz!9mpsa$lpu!20su#a*g^0fzj8e=k#ge=+h$43'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
