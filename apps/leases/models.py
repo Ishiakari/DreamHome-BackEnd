2e98e9534cd4119e87e01989837aab0667b11bf1
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-01 00:23:43 +0800

    update: model config

--- apps/leases/models.py
+++ apps/leases/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class LeaseAgreement(models.Model):
     lease_no = models.CharField(max_length=10, primary_key=True)
     lease_no = models.CharField(max_length=10, primary_key=True)
+    payment_method = models.CharField(max_length=50)
