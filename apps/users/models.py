fae662d295b49a31612a707abbbef59587b0977c
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-31 23:57:59 +0800

    update: model config

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class Staff(models.Model):
     staff_no = models.CharField(max_length=10, primary_key=True)
+    first_name = models.CharField(max_length=100)
     
 
 
