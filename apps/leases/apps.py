d862b405ec723248599c9e7d1229af64dd3278c2
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-31 21:58:29 +0800

    adding requirements

--- apps/leases/apps.py
+++ apps/leases/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class LeasesConfig(AppConfig):
     name = 'leases'
+    name = 'apps.leases'
--- apps/properties/apps.py
+++ apps/properties/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class PropertiesConfig(AppConfig):
     name = 'properties'
+    name = 'apps.properties'
