65d6035a376068b5283f921772c358ca96a9e9b6
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-01 00:26:30 +0800

    update: model config

--- apps/leases/models.py
+++ apps/leases/models.py
@@ -6,3 +6,4 @@ class LeaseAgreement(models.Model):
     monthly_rent = models.DecimalField(max_digits=10, decimal_places=2)
     payment_method = models.CharField(max_length=50)
     deposit = models.DecimalField(max_digits=10, decimal_places=2)
+    deposit_paid = models.BooleanField(default=False)
