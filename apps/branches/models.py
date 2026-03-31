0b86c4bd2b6e98ce88e42f3bc23ff08012d406db
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-31 23:43:16 +0800

    update: model config

--- apps/branches/models.py
+++ apps/branches/models.py
@@ -5,3 +5,4 @@ class Branch(models.Model):
     name = models.CharField(max_length=10, primary_key=True, help_text="Unique branch identifier (e.g., B85)")
     street = models.CharField(max_length=255)
     area = models.CharField(max_length=255, blank=True, null=True)
+    city = models.CharField(max_length=100)
