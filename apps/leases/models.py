0a4da606b81a2282fb54a60b8dd96e318d322e82
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-01 00:25:40 +0800

    update: model config

--- apps/leases/models.py
+++ apps/leases/models.py
@@ -5,3 +5,4 @@ class LeaseAgreement(models.Model):
     lease_no = models.CharField(max_length=10, primary_key=True)
     monthly_rent = models.DecimalField(max_digits=10, decimal_places=2)
     payment_method = models.CharField(max_length=50)
+    deposit = models.DecimalField(max_digits=10, decimal_places=2)
