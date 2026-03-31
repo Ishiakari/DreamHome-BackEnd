29a18736b1bc8b8da42b954056c4c1bd325a55c3
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-31 23:49:19 +0800

    update: model config

--- apps/branches/models.py
+++ apps/branches/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Branch(models.Model):
     name = models.CharField(max_length=10, primary_key=True, help_text="Unique branch identifier (e.g., B85)")
     street = models.CharField(max_length=255)
