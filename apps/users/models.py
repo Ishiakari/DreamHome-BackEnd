de16051637256e478f870e233c5ad1fd6a85292e
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-31 23:51:25 +0800

    update: model config

--- apps/users/models.py
+++ apps/users/models.py
@@ -18,3 +18,4 @@ class PropertyOwner(models.Model):
     owner_no = models.CharField(max_length=10, primary_key=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    address = models.CharField(max_length=255)
