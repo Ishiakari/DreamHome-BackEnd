063335e59b28fb69492fd2dcb32ce03afb1891a1
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-31 23:55:51 +0800

    update: model config

--- apps/users/models.py
+++ apps/users/models.py
@@ -9,6 +9,7 @@ class Staff(models.Model):
 
 class NextOfKin(models.Model):
     staff = models.OneToOneField(Staff, on_delete=models.CASCADE, primary_key=True, related_name='next_of_kin')
+    full_name = models.CharField(max_length=150)
     
 class Renter(models.Model):
     renter_no = models.CharField(max_length=10, primary_key=True)
