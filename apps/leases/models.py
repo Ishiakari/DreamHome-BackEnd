fc7452a136f1a80f03dcf4d4a9e19e0f28c0954a
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-01 00:23:35 +0800

    update: model config

--- apps/leases/models.py
+++ apps/leases/models.py
@@ -3,3 +3,4 @@ from django.db import models
 # Create your models here.
 class LeaseAgreement(models.Model):
     lease_no = models.CharField(max_length=10, primary_key=True)
+    lease_no = models.CharField(max_length=10, primary_key=True)
