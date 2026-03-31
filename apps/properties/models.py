1591d44d0c9a642b2751aed6520505b540db505e
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-01 00:12:37 +0800

    update: model config

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class PropertyForRent(models.Model):
     property_no = models.CharField(max_length=10, primary_key=True)
-    
+    street = models.CharField(max_length=255)
     
 class PropertyViewing(models.Model):
     d = 50
