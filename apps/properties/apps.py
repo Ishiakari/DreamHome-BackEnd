2b3c9f0a7c99cb45d4fa15154ed48e1f62634c9e
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-31 23:28:56 +0800

    update: model config

--- apps/properties/apps.py
+++ apps/properties/apps.py
@@ -1,6 +1,5 @@
 from django.apps import AppConfig
 
-
 class PropertiesConfig(AppConfig):
-    name = 'properties'
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.properties'
