9d0ac0cc7b9898fb03561150078ab0a481f99905
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-03 21:58:59 +0800

    feat: cofig backend

--- apps/users/views.py
+++ apps/users/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Client, Staff
 from .serializers import ClientSerializer, StaffSerializer 
+from rest_framework import permissions
 
 # ==========================================
 # VIEWS (The Doorways)
