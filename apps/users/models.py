d0d07a902eab4a02a40c4e5c09fe11a63e490b10
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-03 17:35:16 +0800

    feat: cofig backend users

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from .utils import generate_client_no
 
 # .\venv\Scripts\activate
 # python manage.py makemigrations
