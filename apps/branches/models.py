9758a12002f185721f69cf06d9b78bd25cceed68
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-04 13:15:22 +0800

    adding serialization

--- apps/branches/models.py
+++ apps/branches/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Branch(models.Model):
-    name = models.CharField(max_length=10, primary_key=True, help_text="Unique branch identifier (e.g., B85)")
+    branch_no = models.CharField(max_length=10, primary_key=True, help_text="Unique branch identifier (e.g., B85)")
     street = models.CharField(max_length=255)
     area = models.CharField(max_length=255, blank=True, null=True)
     city = models.CharField(max_length=100)
