9fe1f06803da3b697af1e16703f3bc5d02cd4865
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-04-04 13:17:07 +0800

    adding serialization

--- apps/branches/views.py
+++ apps/branches/views.py
@@ -1,20 +1,19 @@
 from rest_framework import generics, serializers
-
+from rest_framework.permissions import IsAuthenticated
 from .models import Branch
 
-
 class BranchSerializer(serializers.ModelSerializer):
-	class Meta:
-		model = Branch
-		fields = "__all__"
-
+    class Meta:
+        model = Branch
+        fields = "__all__"
 
 class BranchListCreateView(generics.ListCreateAPIView):
-	queryset = Branch.objects.all()
-	serializer_class = BranchSerializer
-
+    queryset = Branch.objects.all()
+    serializer_class = BranchSerializer
+    permission_classes = [IsAuthenticated] 
 
 class BranchDetailView(generics.RetrieveUpdateDestroyAPIView):
-	queryset = Branch.objects.all()
-	serializer_class = BranchSerializer
-	lookup_field = "name"
+    queryset = Branch.objects.all()
+    serializer_class = BranchSerializer
+    lookup_field = "branch_no"
+    permission_classes = [IsAuthenticated]
