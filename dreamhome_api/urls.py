f61649bbea8f515106deb0e34882d01216c73e9f
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-05-02 20:35:47 +0800

    Add missing payments URL path to urlpatterns

--- dreamhome_api/urls.py
+++ dreamhome_api/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('api/branches/', include('apps.branches.urls')),
     path('api/properties/', include('apps.properties.urls')),
     path('api/leases/', include('apps.leases.urls')),
+    path('api/payments/', include('apps.payments.urls')),
     
     path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
